f066adcaa53bb4682d69ca271b466a369f6aa824
ravindra <ravibalajipawar@gmail.com>
2022-06-05 22:56:31 +0530

    Initial commit

--- Examples/src/com/java/examples/StreamExamples.java
+++ Examples/src/com/java/examples/StreamExamples.java
@@ -13,10 +13,12 @@ import java.util.List;
 public class StreamExamples {
 
 	/**
-	 * find all even numbers in a list
+	 * find all even numbers in a list 
 	 */
 	public static void main(String[] args) {
 		List<Integer> myList = Arrays.asList(10, 12, 15, 17, 18, 4, 6, 3);
 		myList.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+		
+		System.out.println("hello");
 	}
 }
